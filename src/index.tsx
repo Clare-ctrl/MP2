4d79862ce46b02484fc1bf6f5ae36b772a4aae9d
Mengmeng Fang <mengmengfang@Mengmengs-MacBook-Air.local>
2025-10-08 15:49:51 -0500

    Fix and try

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './routes/App';
+import App from './App';
 import { RouterProvider, createBrowserRouter} from 'react-router-dom';
 import CarouselView from './routes/CarouselView';
 import TabSection from './components/TabSection';
